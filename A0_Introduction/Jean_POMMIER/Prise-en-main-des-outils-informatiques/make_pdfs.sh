b067df73a282ece8529a68cfd000b56b87606495
CDXX710 <139405089+CDXX710@users.noreply.github.com>
2024-12-12 12:52:21 +0100

    Delete A0_Introduction/Jean_POMMIER/Prise-en-main-des-outils-informatiques/make_pdfs.sh

--- A0_Introduction/Jean_POMMIER/Prise-en-main-des-outils-informatiques/make_pdfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pandoc -V geometry:margin=35mm -o programme.pdf programme.md
-pandoc -V geometry:margin=35mm -o notes.pdf notes.md
-pandoc -V geometry:margin=35mm -o formationPriseEnMain.pdf readme.md \
-    clavier/readme.md \
-    windows/readme.md \
-    linux/readme.md \
-    BD/readme.md
